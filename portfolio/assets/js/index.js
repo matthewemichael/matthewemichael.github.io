baf6ac9cf23f6b677b0d1c2c7c7e064cd94170e6
Matt Michael <matthewemichael@gmail.com>
2019-11-21 23:04:24 -0600

    updated portfolio, uploaded new portfolio - wip

---
+++ portfolio/assets/js/index.js
@@ -0,0 +1,27 @@
+$('.nav').click(function() {
+    var id = $(this).attr('id');
+    $('html, body').animate({
+        scrollTop: ($('#' + id + '.section').offset().top)
+    }, 1000);
+});
+
+$('.down-arrow').click(function() {
+    var id = $(this).attr('id');
+    $('html, body').animate({
+        scrollTop: ($('#' + id + '.section').offset().top)
+    }, 1000);
+});
+
+$('.navbar-nav>li>a').on('click', function(){
+    $('.navbar-collapse').collapse('hide');
+});
+
+$(document).ready(function () {
+    $(document).click(function (event) {
+        var clickover = $(event.target);
+        var _opened = $(".navbar-collapse").hasClass("show");
+        if (_opened === true && !clickover.hasClass("navbar-toggler")) {
+            $(".navbar-toggler").click();
+        }
+    });
+});
